6c979f57fe025d1f62529407b78649747bd15662
Yuvi3007 <yuvrajingale1111@gmail.com>
2025-02-04 10:54:14 +0000

    dataypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+
+//alert("3+3") //we are using node js ,not browser
+
+console.log(3+
+    4) //code readability should be higher
+
+console.log("yuvi")
+
+let age =20
+let isLoggedIn =false
+let state ;
+
+//number => 2 to power 53
+//bigint
+//string  => ""
+//boolean => true/false
+//null => stand alone value 
+//undefined => 
+//symbol => unique
+
+//object 
+
+console.log(typeof undefined) //undefined 
+
+console.log(typeof null) //object null is an object interview question
