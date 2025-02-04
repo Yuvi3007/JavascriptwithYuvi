ffde3796f9c0c4f20d4f9c768b61a36462b5fb4c
Yuvi3007 <yuvrajingale1111@gmail.com>
2025-02-04 12:04:14 +0000

    conversionoperation

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,39 @@
+let score  ="33abc"
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+let valueInNum = Number (score)
+//console.log(typeof valueInNum)
+//console.log(valueInNum )
+
+//when assigned score =33 console showed it as a number 
+// its value was 33 before adding abc to the end 
+
+//after conversion 
+
+//when abc was addded to the tail of 33 although it converted type into number 
+//but the output was NaN remember this
+
+let someNum=45
+let stringNum =String(someNum)
+//console.log(stringNum)
+///console.log(typeof(stringNum))
+//num=>string above
+
+
+let isLogin =0
+let booleanislogin =Boolean(isLogin)
+//console.log(booleanislogin)
+
+// using boolean and converting them 
+// 0=> false & 1=> true
+
+
+//operations 
+//assigning negative values
+let value =7
+let negValue =-value
+console.log(negValue)
+
+console.table([2+2,3-1,2*3,3/2,4%2]);
