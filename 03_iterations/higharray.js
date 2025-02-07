4bcb2296fbbf67775260c8b414c62ed87f8a69c0
Yuvi3007 <yuvrajingale1111@gmail.com>
2025-02-07 15:35:23 +0000

    High order array

--- 03_iterations/higharray.js
+++ 03_iterations/higharray.js
@@ -3,5 +3,25 @@
 const arr=[1,2,3,4,5];
 
 for(const num of arr){
-    console.log(num);
+    //console.log(num);
 }
+
+const greetings = "Hello World!";
+for(const greet of greetings){
+    //console.log(`Each char is ${greet}`);
+}
+
+//maps in javascript=>
+    /*Map objects are collections of key-value pairs. 
+A key in the Map may only occur once; 
+it is unique in the Map's collection.
+*/ 
+
+const map =new Map();
+map.set('IN',"India");
+map.set('FR',"France");
+map.set('USA', "America");
+console.log(map);
+
+
+
